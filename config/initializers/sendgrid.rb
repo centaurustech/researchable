050c6276e337bca1ff28a0fc234fbece6c1a0b51
Tyler Horan <tylerhoran@gmail.com>
2012-09-18 12:24:18 -0400

    sendgrid info

--- config/initializers/sendgrid.rb
+++ config/initializers/sendgrid.rb
@@ -4,8 +4,8 @@ begin
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => 'catarse',
-    :password       =>  Configuration[:sendgrid],
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com'
     }
     ActionMailer::Base.delivery_method = :smtp
